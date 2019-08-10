8efa97d690744e2a9b6ecd5964012c2f6fc73852
_imQuang <nguyenquang119892gmail.com>
2019-08-10 09:43:17 +0700

    add function callback

--- Cat.js
+++ Cat.js
@@ -1,4 +1,12 @@
-function Cat(){
-
+function sumAndDo(numbers,callback){
+	let array=[];
+	for(let i=0;i<numbers.length;i++){
+		array.push(callback(numbers[i]));
+	}
+	return array;
 }
-module.exports=Cat;
+
+function Do(num){
+	return num*2;
+}
+sumAndDo([1,2,3],Do);
